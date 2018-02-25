3c7909f722bb5e49292eeff39e5a1dc9d6862957
wrma <632498767@qq.com>
2018-02-25 21:00:17 +0800

    初始化

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+/*
+* @Author: wrma
+* @Date:   2018-02-24 20:17:16
+* @Last Modified by:   wrma
+* @Last Modified time: 2018-02-24 20:54:11
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+ReactDOM.render(
+	<div>
+		<h1>Hello, world!</h1>
+	</div>,
+  document.getElementById('app')
+);
